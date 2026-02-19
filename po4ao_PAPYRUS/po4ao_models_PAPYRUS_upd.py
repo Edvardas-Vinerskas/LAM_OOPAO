bf6dd2fb3f5d4baa762815c9794e9eef7bde0615
evinerskas <vinedw9@gmail.com>
2026-02-19 17:35:52 +0100

    19/02/2025

--- po4ao_PAPYRUS/po4ao_models_PAPYRUS_upd.py
+++ po4ao_PAPYRUS/po4ao_models_PAPYRUS_upd.py
@@ -7,7 +7,7 @@ import time
 # import matplotlib.pyplot as plt
 from po4ao_PAPYRUS.po4ao_config_PAPYRUS import config
 
-n_filt = config['NN_models']['filters_per_layer']
+n_filt = config['NN_models']['filters_per_layer'] #32
 
 
 class ConvDynamics(nn.Module):
